871f2dad62ced1fbc4b1cdee071a9aaaf8bcb896
Wojciech Wojcik <wojciv97@gmail.com>
2022-10-30 17:59:44 +0100

    project mouse move in react

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Animated from "./Animated";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="balloon-animation">
+          <Animated
+            className="cloud"
+            src="/cloud.png"
+            ratioX="0.36"
+            ratioY="0.34"
+          />
+          <Animated
+            className="cloud"
+            src="/cloud.png"
+            ratioX="0.36"
+            ratioY="0.34"
+          />
+          <Animated
+            className="cloud"
+            src="/cloud.png"
+            ratioX="0.16"
+            ratioY="0.24"
+          />
+          <Animated
+            className="cloud"
+            src="/cloud.png"
+            ratioX="0.56"
+            ratioY="0.24"
+          />
+          <Animated
+            className="cloud"
+            src="/cloud.png"
+            ratioX="0.26"
+            ratioY="0.44"
+          />
+          <Animated
+            className="baloon"
+            src="/balloon.png"
+            ratioX="0.26"
+            ratioY="0.34"
+          />
+        </div>
       </header>
     </div>
   );
